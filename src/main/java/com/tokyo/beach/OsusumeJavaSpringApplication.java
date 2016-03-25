5c5f23a3fceec1a2ae4804325d051c314400752f
Chiharu Miya <cmiya@pivotal.io>
2016-03-25 10:21:50 +0900

    Initial Commit.
    Signed-off-by: Kyle Gearhart <kgearhart@pivotal.io>

---
+++ src/main/java/com/tokyo/beach/OsusumeJavaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.tokyo.beach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsusumeJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsusumeJavaSpringApplication.class, args);
+	}
+}
