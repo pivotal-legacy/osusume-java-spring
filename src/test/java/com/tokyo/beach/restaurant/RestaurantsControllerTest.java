f654a389523c2138291ce16b846994e2219d1eb5
Chiharu Miya <cmiya@pivotal.io>
2016-04-04 10:49:43 +0900

    Simple clean-up.
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- src/test/java/com/tokyo/beach/restaurant/RestaurantsControllerTest.java
+++ src/test/java/com/tokyo/beach/restaurant/RestaurantsControllerTest.java
@@ -1,68 +1,67 @@
 package com.tokyo.beach.restaurant;
 
-import com.tokyo.beach.application.restaurant.*;
-import com.tokyo.beach.application.photos.NewPhotoUrl;
 import com.tokyo.beach.application.photos.PhotoUrl;
+import com.tokyo.beach.application.restaurant.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class RestaurantsControllerTest {
-    RestaurantRepository mockRestaurantRepository;
-    DetailedRestaurantRepository mockDetailedRestaurantRepository;
-    RestaurantsController restaurantsController;
-    MockMvc mockMvc;
+    private RestaurantRepository mockRestaurantRepository;
+    private DetailedRestaurantRepository mockDetailedRestaurantRepository;
+    private MockMvc mockMvc;
 
     @Before
     public void setUp() {
         mockRestaurantRepository = mock(RestaurantRepository.class);
         mockDetailedRestaurantRepository = mock(DetailedRestaurantRepository.class);
-        restaurantsController = new RestaurantsController(mockRestaurantRepository, mockDetailedRestaurantRepository);
-        mockMvc = MockMvcBuilders.standaloneSetup(restaurantsController)
-                .build();
+        RestaurantsController restaurantsController = new RestaurantsController(
+                mockRestaurantRepository,
+                mockDetailedRestaurantRepository
+        );
+
+        mockMvc = standaloneSetup(restaurantsController).build();
     }
 
     @Test
     public void testGettingAListOfRestaurants() throws Exception {
         when(mockRestaurantRepository.getAll()).thenReturn(
-            Collections.singletonList(
-                    new Restaurant(
-                            1,
-                            "Afuri",
-                            "Roppongi",
-                            false,
-                            true,
-                            false,
-                            "",
-                            null
-                    )
-            )
+                singletonList(
+                        new Restaurant(
+                                1,
+                                "Afuri",
+                                "Roppongi",
+                                false,
+                                true,
+                                false,
+                                "",
+                                emptyList()
+                        )
+                )
         );
 
 
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get("/restaurants"));
-
-
-        result.andExpect(MockMvcResultMatchers.status().isOk());
-        result.andExpect(MockMvcResultMatchers.jsonPath("$[0].id", equalTo(1)));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$[0].name", equalTo("Afuri")));
+        mockMvc.perform(get("/restaurants"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", equalTo(1)))
+                .andExpect(jsonPath("$[0].name", equalTo("Afuri")));
     }
 
     @Test
     public void testCreatingARestaurant() throws Exception {
-        ArrayList<NewPhotoUrl> afuriNewPhotoUrls = new ArrayList<>();
         NewRestaurant afuriNewRestaurant = new NewRestaurant(
                 "Afuri",
                 "Roppongi",
@@ -70,7 +69,7 @@ public class RestaurantsControllerTest {
                 true,
                 false,
                 "",
-                afuriNewPhotoUrls
+                emptyList()
         );
         when(mockRestaurantRepository.createRestaurant(afuriNewRestaurant)).thenReturn(
                 new Restaurant(
@@ -81,22 +80,21 @@ public class RestaurantsControllerTest {
                         true,
                         false,
                         "",
-                        null
+                        emptyList()
                 )
         );
 
 
-
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/restaurants")
-                .contentType("application/json")
-                .content("{\"name\":\"Afuri\"}"));
-
-
-        result.andExpect(MockMvcResultMatchers.status().isCreated());
+        mockMvc.perform(
+                post("/restaurants")
+                        .contentType(APPLICATION_JSON_UTF8_VALUE)
+                        .content("{\"name\":\"Afuri\"}")
+        )
+                .andExpect(status().isCreated());
     }
 
     @Test
-    public void testGetRestaurantWithoutPhotUrls() throws Exception {
+    public void testGetRestaurantWithoutPhotoUrls() throws Exception {
         when(mockDetailedRestaurantRepository.getRestaurant("1")).thenReturn(
                 new Restaurant(
                         1,
@@ -106,25 +104,20 @@ public class RestaurantsControllerTest {
                         true,
                         false,
                         "",
-                        new ArrayList<PhotoUrl>()
+                        emptyList()
                 )
         );
 
 
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get("/restaurants/1"));
-
-
-        result.andExpect(MockMvcResultMatchers.status().isOk());
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.id", equalTo(1)));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.name", equalTo("Afuri")));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.photo_urls", equalTo(new ArrayList<PhotoUrl>())));
+        mockMvc.perform(get("/restaurants/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(1)))
+                .andExpect(jsonPath("$.name", equalTo("Afuri")))
+                .andExpect(jsonPath("$.photo_urls", equalTo(emptyList())));
     }
 
     @Test
     public void testGetRestaurantWithPhotoUrls() throws Exception {
-        List<PhotoUrl> photoUrls = new ArrayList<PhotoUrl>();
-        photoUrls.add(new PhotoUrl(1, "Url1", 1));
-        photoUrls.add(new PhotoUrl(2, "Url2", 1));
         when(mockDetailedRestaurantRepository.getRestaurant("1")).thenReturn(
                 new Restaurant(
                         1,
@@ -134,20 +127,17 @@ public class RestaurantsControllerTest {
                         true,
                         false,
                         "",
-                        photoUrls
+                        asList(new PhotoUrl(1, "Url1", 1), new PhotoUrl(2, "Url2", 1))
                 )
         );
 
 
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get("/restaurants/1"));
-
-
-        result.andExpect(MockMvcResultMatchers.status().isOk());
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.id", equalTo(1)));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.name", equalTo("Afuri")));
-
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.photo_urls[0].url", equalTo("Url1")));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.photo_urls[1].url", equalTo("Url2")));
+        mockMvc.perform(get("/restaurants/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(1)))
+                .andExpect(jsonPath("$.name", equalTo("Afuri")))
+                .andExpect(jsonPath("$.photo_urls[0].url", equalTo("Url1")))
+                .andExpect(jsonPath("$.photo_urls[1].url", equalTo("Url2")));
 
     }
 }
