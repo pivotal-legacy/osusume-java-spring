460620fa506a339bac5a28bde06fa9ecb132a7f0
Chiharu Miya <cmiya@pivotal.io>
2016-05-31 17:26:32 +0900

    Add CrossOrigin Annotation to PriceRange Controller.
    [#120253517]
    
    Signed-off-by: Robert Gravina <rgravina@pivotal.io>

--- components/restaurants/src/main/java/com/tokyo/beach/restaurants/pricerange/PriceRangeController.java
+++ components/restaurants/src/main/java/com/tokyo/beach/restaurants/pricerange/PriceRangeController.java
@@ -2,6 +2,7 @@ package com.tokyo.beach.restaurants.pricerange;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
+@CrossOrigin
 public class PriceRangeController {
     private PriceRangeRepository priceRangeRepository;
 
