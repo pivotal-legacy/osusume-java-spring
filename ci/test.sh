d2c9ba7031757220f7e9dc8e71d4518021655a22
Andrew Wright <anwright@pivotal.io>
2016-08-10 17:38:30 +0900

    Setup database before running unit tests on concourse
    Signed-off-by: Yuki Nishijima <ynishijima@pivotal.io>

--- ci/test.sh
+++ ci/test.sh
@@ -3,5 +3,11 @@
 set -e -x
 
 pushd osusume-java-spring
-    TERM=dumb OSUSUME_DATABASE_URL=jdbc:postgresql://localhost/osusume-dev ./gradlew clean test build
+    dropdb --if-exists -e osusume-test
+    createdb -e osusume-test
+    ./gradlew flywayMigrate
+    ./gradlew build
+OSUSUME_DATABASE_URL=jdbc:postgresql://localhost/osusume-test java -jar build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar &
+JAVA_SERVER_PID=$!
+    TERM=dumb ./gradlew clean test build
 popd
