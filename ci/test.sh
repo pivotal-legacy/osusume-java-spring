b75509737d9b7096500f58d5a5f38f400cb35c0b
Andrew Wright <anwright@pivotal.io>
2016-08-10 17:25:44 +0900

    Add postgres environment variable to councourse ci script
    Signed-off-by: Yuki Nishijima <ynishijima@pivotal.io>

--- ci/test.sh
+++ ci/test.sh
@@ -3,5 +3,5 @@
 set -e -x
 
 pushd osusume-java-spring
-    TERM=dumb ./gradlew clean test build
+    TERM=dumb OSUSUME_DATABASE_URL=jdbc:postgresql://localhost/osusume-dev ./gradlew clean test build
 popd
