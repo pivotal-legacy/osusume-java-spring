4b1edfd35df97d388b018a4e0bcd9403408b6eb2
Andrew Wright <anwright@pivotal.io>
2016-08-12 16:46:13 +0900

    Fix syntax

--- ci/test.sh
+++ ci/test.sh
@@ -5,11 +5,11 @@ set -e -x
 pushd osusume-java-spring
     DATA_DIR=/var/lib/pgsql/data
     su postgres -c "pg_ctl -D $DATA_DIR -l ${DATA_DIR}/logfile start"
-    su postgres -c dropdb --if-exists -e osusume-test
-    su postgres -c createdb -e osusume-test
-    su postgres -c ./gradlew flywayMigrate
-    su postgres -c ./gradlew build
-OSUSUME_DATABASE_URL=jdbc:postgresql://localhost/osusume-test java -jar build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar &
-JAVA_SERVER_PID=$!
-    su postgres -c TERM=dumb ./gradlew clean test build
+    su postgres -c "dropdb --if-exists -e osusume-test"
+    su postgres -c "createdb -e osusume-test"
+    su postgres -c "./gradlew flywayMigrate"
+    su postgres -c "./gradlew build"
+    su postgres -c "OSUSUME_DATABASE_URL=jdbc:postgresql://localhost/osusume-test java -jar build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar &
+JAVA_SERVER_PID=$!"
+    su postgres -c "TERM=dumb ./gradlew clean test build"
 popd
