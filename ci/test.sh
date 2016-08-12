54f5f20dc48436f300f32949dbc7f67cb08cc6d1
Andrew Wright <anwright@pivotal.io>
2016-08-12 17:00:34 +0900

    Use the osusume-test database for the migration

--- ci/test.sh
+++ ci/test.sh
@@ -9,7 +9,7 @@ pushd osusume-java-spring
     sleep 10
     su postgres -c "dropdb --if-exists -e osusume-test"
     su postgres -c "createdb -e osusume-test"
-    su postgres -c "./gradlew flywayMigrate"
+    su postgres -c "OSUSUME_DATABASE_URL=jdbc:postgresql://localhost/osusume-test ./gradlew flywayMigrate"
     su postgres -c "./gradlew build"
     su postgres -c "OSUSUME_DATABASE_URL=jdbc:postgresql://localhost/osusume-test java -jar build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar &
 JAVA_SERVER_PID=$!"
